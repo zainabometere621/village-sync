bbb8fe87e0b80982551f8e573745ee2f7c5ce548
Zainab <zainabometere621@gmail.com>
2025-07-27 15:03:13 +0930

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import image from "../image/slack.png"
 import haleemah from "../image/discord.png"
 import roqeeb from "../image/git.png"
 import zainab from "../image/amazon1.png"
-
+import hawa from "../image/tesla.png"
 import HeroSection from "./components/HeroSection"
 import Thanks from "./components/Thanks"
 
@@ -47,16 +47,12 @@ export default function App() {
 
     <div className='flex flex-col gap-10 md:flex-row md:gap-20 justify-center '>
       <HeroSection 
-        name= "Sharifiyu Roqeeb Taiwo"
-        image= {roqeeb}
-        link= "https://roqeeb-dev.github.io/github-clone/"
+        name= "Owonikoko Hawawu Olanike"
+        image= {hawa}
+        link= "https://tesla-page-silk.vercel.app/"
       />
 
-      <HeroSection 
-        name= "Abdulganiyu Zainab Ometere"
-        image= {zainab}
-        link= "https://amazonclone-self-ten.vercel.app/"
-      />
+      
 
       
       
